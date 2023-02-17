6340a8cb373acba74cdb9a1ac43784af6162ef7e
Antony Beardsmore <sider@me.com>
2023-02-17 18:22:26 +0000

    Finished DemoApp

---
+++ GenericsAndEventsCSV/DataAccess.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace GenericsAndEventsCSV
+{
+    public class DataAccess<T> where T : new()
+    {
+
+        public event EventHandler<T> BadEntryFound;
+
+        // Extension method making use of generic types
+        public void SaveToCSV(List<T> items, string filePath) // T has to have an empty constructor
+        {
+            List<string> rows = new List<string>();
+            T entry = new T();
+            var cols = entry.GetType().GetProperties();
+
+            string row = "";
+
+            // Headers
+            foreach (var col in cols)
+            {
+                row += $",{col.Name}";
+            }
+
+            row = row.Substring(1); // ignore first comma
+            rows.Add(row);
+
+            // Loop through each object 
+            foreach (var item in items)
+            {
+                row = "";
+                bool naughtyWordDetected = false;
+
+                // get values in each object 
+                foreach (var col in cols)
+                {
+                    // Check for bad data (naughty words)
+                    string val = col.GetValue(item, null).ToString(); // Reflection used here
+                    naughtyWordDetected = NaughtyWordDetector(val);
+
+                    if (naughtyWordDetected == true)
+                    {
+                        BadEntryFound?.Invoke(this, item);
+                        break;
+                    }
+
+                    row += $",{ val }";
+                }
+
+                if (naughtyWordDetected == false)
+                {
+                    row = row.Substring(1); // ignore first comma
+                    rows.Add(row);
+                }
+            }
+
+            File.WriteAllLines(filePath, rows);
+
+        }
+
+        private bool NaughtyWordDetector(string stringToTest)
+        {
+            bool output = false;
+            string lowerCaseTest = stringToTest.ToLower();
+
+
+            if (lowerCaseTest.Contains("bloody") || lowerCaseTest.Contains("blimey"))
+            {
+                output = true;
+            }
+
+            return output;
+        }
+    }
+}
--- GenericsAndEventsCSV/Program.cs
+++ GenericsAndEventsCSV/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GenericsAndEventsCSV
 {
@@ -12,23 +9,38 @@ namespace GenericsAndEventsCSV
         {
             List<PersonModel> people = new List<PersonModel>
             {
-                new PersonModel{ FirstName = "Antony", LastName = "Beardsmore", Email = "Antony@me.com"},
+                new PersonModel{ FirstName = "Antony", LastName = "BeardHorn", Email = "Antony@me.com"},
                 new PersonModel{ FirstName = "Steve", LastName = "Lemons", Email = "Steve@dogs.net"},
-                new PersonModel{ FirstName = "William", LastName = "Riker", Email = "William@enterprise.com"},
+                new PersonModel{ FirstName = "William", LastName = "bloodyhell", Email = "William@enterprise.com"}, // bad data row
                 new PersonModel{ FirstName = "Brian", LastName = "May", Email = "Brian@queen.co.uk"}
             };
 
             List<CarModel> cars = new List<CarModel>
             {
-                new CarModel{Manufacturer = "VW", Model = "Golf"},
+                new CarModel{Manufacturer = "VW", Model = "Golfblimey"}, // bad data row
                 new CarModel{Manufacturer = "Audi", Model = "A4"},
                 new CarModel{Manufacturer = "Toyota", Model = "Auris"},
             };
 
+            DataAccess<PersonModel> peopleData = new DataAccess<PersonModel>();
+            peopleData.BadEntryFound += PeopleData_BadEntryFound;
+            peopleData.SaveToCSV(people, @"D:\temp\CSV_files\people.csv");
+
+            DataAccess<CarModel> carData = new DataAccess<CarModel>();
+            carData.BadEntryFound += CarData_BadEntryFound;
+            carData.SaveToCSV(cars, @"D:\temp\CSV_files\cars.csv");
 
             Console.ReadLine();
         }
+
+        private static void CarData_BadEntryFound(object sender, CarModel e)
+        {
+            Console.WriteLine($"Bad entry found for {e.Manufacturer} {e.Model}");
+        }
+
+        private static void PeopleData_BadEntryFound(object sender, PersonModel e)
+        {
+            Console.WriteLine($"Bad entry found for {e.FirstName} {e.LastName}");
+        }
     }
-
-
 }
